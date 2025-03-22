ec6534d453202d7ccddd277f6a886154c158816d
Funfey <sara0gri@gmail.com>
2025-03-22 21:48:42 +0100

    commented date checking

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.Date;
 
 public class Main extends Finestra {
     public static void main (String args[]){
-    	LocalDateTime.now();
+    	//LocalDateTime.now();
         Finestra mynameisgiovannigiorgiobuteveryonecallsmegiorgio= new Finestra();
     }
 }
